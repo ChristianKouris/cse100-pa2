ffc4810a5885811ffd0259364d579b2adae0947f
Christian Kouris <ckouris@ucsd.edu>
2019-10-23 23:12:57 -0700

    Added test cases for Trie

--- test/test_DictionaryTrie.cpp
+++ test/test_DictionaryTrie.cpp
@@ -24,3 +24,20 @@ TEST(DictTrieTests, EMPTY_TEST) {
     ASSERT_EQ(dict.find("abrakadabra"), false);
 }
 
+TEST(DictTrieTests, INSERT_TEST) {
+    DictionaryTrie dict;
+    dict.insert("help", 100);
+    ASSERT_EQ(dict.find("help"), true);
+}
+
+TEST(DictTrieTests, INCORRECT_FIND_TEST) {
+    DictionaryTrie dict;
+    dict.insert("animal", 100);
+    ASSERT_EQ(dict.find("an"), false);
+}
+
+TEST(DictTrieTests, REINSERT_TEST) {
+    DictionaryTrie dict;
+    dict.insert("animal", 100);
+    ASSERT_EQ(dict.insert("animal", 15), false);
+}
